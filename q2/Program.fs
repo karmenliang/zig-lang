641d585b9e55adb69455764ee180863159eeb2a8
Karmen <karmennliang@gmail.com>
2018-11-13 15:59:08 -0500

    implemented cmd line input

--- q2/Program.fs
+++ q2/Program.fs
@@ -4,31 +4,23 @@ open LambdaParser
 open AlphaReduction
 open CS334
 
+let usage() =
+    printfn "Usage: dotnet run <s>, where s is a lambda expression"
+    exit 1
+
+let argparse argv =
+    if Array.length argv <> 1 then usage()
+    let n =
+        try string argv.[0]
+        with
+        | :? System.FormatException as e ->
+            usage()   
+    n
+
 [<EntryPoint>]
 let main argv =
-    // testing sub #1
-    let v = 'x'
-    let with_e = Variable 'y'
-    let in_e = Application(Variable 'x', Variable 'x')
-    printfn "%s" (lambdaprint (sub v with_e in_e))
-
-    // testing sub #2
-    let v = 'x'
-    let with_e = Variable 'y'
-    let in_e = Abstraction('z', Application(Variable 'z', Variable 'x'))
-    printfn "%s" (lambdaprint (sub v with_e in_e))
-
-    // testing betastep #1: e = betastep e, where e is beta-normal
-    let e1 = Abstraction('x', Variable 'x') // (Lx.x)
-    printfn "%s" (lambdaprint (betastep e1))
-
-    // testing betastep #2:
-    let e2 = Application(Abstraction('x',Application(Variable 'x', Variable 'y')), Abstraction('z', Application(Variable 'z', Variable 'y'))) // ((Lx.(xy))(Lz.(zy)))
-    // one beta step should give us ((Lz.(zy))y)
-    printfn "%s" (lambdaprint (betastep e2))
-
-    // testing betanorm #1: using e from above ((Lx.(xy))(Lz.(zy)))
-    // beta-normal form is (yy)
-    printfn "%s" (lambdaprint (betanorm e2))
-
+    let input = parse (argparse argv)
+    match input with
+    | Some expr -> printfn "%A" (lambdaprint (betanorm expr))
+    | None -> printfn "Invalid expression!"
     0
