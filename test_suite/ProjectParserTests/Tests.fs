b902fa9152231606646abae53c554a5daafa5c9d
Karmen <karmennliang@gmail.com>
2018-12-09 21:08:04 -0500

    added seq test

--- test_suite/ProjectParserTests/Tests.fs
+++ test_suite/ProjectParserTests/Tests.fs
@@ -2,10 +2,18 @@
 
 open System
 open Microsoft.VisualStudio.TestTools.UnitTesting
+open ProjectParser
 
 [<TestClass>]
 type TestClass () =
 
     [<TestMethod>]
-    member this.TestMethodPassing () =
-        Assert.IsTrue(true);
+    member this.SeqParses () =
+        let input = "ahead 50; cw 90; ahead 50"
+        let expected = Seq (Ahead (NumExpr 50),Seq (Clockwise (NumExpr 90),Ahead (NumExpr 50)))
+        let result = parse input
+        match result with
+        | Some ws ->
+            Assert.AreEqual(expected, ws)
+        | None ->
+            Assert.IsTrue false
