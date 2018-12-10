14a227d9e212ecc5cad811a059b25dcc9ded1d40
Karmen <karmennliang@gmail.com>
2018-12-09 21:31:37 -0500

    added penrgb test

--- test_suite/ProjectInterpreterTests/Tests.fs
+++ test_suite/ProjectInterpreterTests/Tests.fs
@@ -2,10 +2,16 @@
 
 open System
 open Microsoft.VisualStudio.TestTools.UnitTesting
+open ProjectParser
+open ProjectInterpreter
 
 [<TestClass>]
 type InterpreterTestClass () =
 
     [<TestMethod>]
-    member this.TestMethodPassing () =
-        Assert.IsTrue(true);
+    member this.PencolorChangesState () =
+        let input = PenRGB ((NumExpr 150),(NumExpr 100),(NumExpr 10))
+        let expected = State(List.empty,Turtle(300,200,(PI/2.0)),Pen(1,(150,100,10),true),Map.empty,Boundaries(Dimensions(600,400),Home(300,200)))
+        let origstate = State(List.empty,Turtle(300,200,(PI/2.0)),Pen(1,(0,0,0),true),Map.empty,Boundaries(Dimensions(600,400),Home(300,200)))
+        let result = eval input origstate
+        Assert.AreEqual(expected, result)
